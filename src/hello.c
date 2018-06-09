95e8e41a84ddc290306312941e2b120322a5c6c5
root-ar-pc <40126451+root-ar-pc@users.noreply.github.com>
2018-06-10 00:46:16 +0500

    Create hello.c
    contain main function to fulfull everything of this a small hello world file.

---
+++ src/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <signal.h>
+
+void good_bye(int SIGNUM) {
+        printf("Thanks, you let me go. Good bye.\n");
+        exit(0);
+}
+
+int main() {
+
+        connect(SIGNUM, good_bye);
+
+        while(1) {
+        printf("I am getting crazy to say you \"Hello World\" . Let me getout by pressing CTRL+C.\n");
+        }
+        return 0;
+}
